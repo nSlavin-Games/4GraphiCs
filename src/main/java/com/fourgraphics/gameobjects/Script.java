814ef635b311e04fac9ef4abcf3a109c1bc74c9f
Paggios <66666964+Paggios@users.noreply.github.com>
2021-04-30 11:55:38 +0200

    Correzione information hiding Script
    I metodi della classe Script diventano pubblici

--- src/main/java/com/fourgraphics/gameobjects/Script.java
+++ src/main/java/com/fourgraphics/gameobjects/Script.java
@@ -3,10 +3,13 @@ package com.fourgraphics.gameobjects;
 import com.fourgraphics.colliders.Collider;
 
 public class Script {
+
     public GameObject gameObject; //l’oggetto a cui è attaccato questo componente
     public Transform transform; //: la posizione e la dimensione dell’oggetto a cui è attaccato questo componente
 
-    void Start() {} //il metodo che viene eseguito al caricamento della scena
-    void Update() {} //il metodo che viene eseguito ogni frame
-    void OnCollisionEnter(Collider self, Collider other) {} //eseguito quando l’oggetto entra in collisione con qualcosa
+    public void Start() {} //il metodo che viene eseguito al caricamento della scena
+    public void Update() {} //il metodo che viene eseguito ogni frame
+    public void OnCollisionEnter(Collider self, Collider other) {} //eseguito quando l’oggetto entra in collisione con qualcosa
+
+
 }
