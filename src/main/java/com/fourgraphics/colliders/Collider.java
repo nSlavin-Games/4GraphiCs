04df7bc4f329d3ca3ef4341537559026e9994d31
Lorenzo-Mercurio <Lore03discord@gmail.com>
2021-05-13 16:38:20 +0200

    Gruppo collisions

--- src/main/java/com/fourgraphics/colliders/Collider.java
+++ src/main/java/com/fourgraphics/colliders/Collider.java
@@ -1,4 +1,63 @@
 package com.fourgraphics.colliders;
 
-public class Collider {
+import com.fourgraphics.gameobjects.GameObject;
+import com.fourgraphics.gameobjects.Vector2;
+import com.fourgraphics.scenemanagement.SceneManager;
+
+import processing.core.PApplet;
+
+import com.fourgraphics.gameobjects.Transform;
+
+abstract public class Collider {
+	
+	public GameObject gameObject;
+	Vector2 previousPosition;
+	public Transform  transform;
+	private boolean dynamicObject;
+	private boolean debug;
+	protected PApplet sketch;
+	
+	//SceneManager.getApp()
+	
+	public Collider(boolean dynamicObject) {
+		
+		setDynamic(dynamicObject);
+		sketch=SceneManager.getApp();
+	}
+	
+	public Collider(boolean dynamicObject, PApplet sketch) {
+		
+		setDynamic(dynamicObject);
+		this.sketch=sketch;
+	}
+	
+	
+	abstract CollisionDirection CheckCollision(Collider other);
+	
+	abstract CollisionDirection CheckCollisionSnap(Collider other);
+
+
+	public boolean isDynamic() {
+		return dynamicObject;
+	}
+
+
+	public void setDynamic(boolean dynamicObject) {
+		this.dynamicObject = dynamicObject;
+	}
+
+
+	public boolean isDebug() {
+		return debug;
+	}
+
+
+	public void setDebug(boolean debug) {
+		this.debug = debug;
+	}
+	
+	
+	
+	
+
 }
