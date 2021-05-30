bceb0693f763bff9ec4896a227524eb8bbd67d35
dfmolinari <66701511+dfmolinari@users.noreply.github.com>
2021-05-30 02:09:32 +0200

    Basic window setup
    Started SceneManager writing by setting up a window

---
+++ src/main/java/com/fourgraphics/scenes/SceneManager.java
@@ -0,0 +1,115 @@
+package com.fourgraphics.scenes;
+
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public class SceneManager
+{
+    private static long window;
+
+    private static boolean debugMode;
+
+    public static void run(int width, int height, String title, boolean debug)
+    {
+        debugMode = debug;
+        start(width, height, title);
+        update();
+
+        // Free the window callbacks and destroy the window
+        glfwFreeCallbacks(window);
+        glfwDestroyWindow(window);
+
+        // Terminate GLFW and free the error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    private static void start(int width, int height, String title)
+    {
+        // Setup an error callback. The default implementation
+        // will print the error message in System.err.
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        if(!glfwInit())
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        //Configure GLFW
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); //window hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); //window resizable
+
+        //Creating the window
+        window = glfwCreateWindow(width,height,title, NULL, NULL);
+        if(window == NULL)
+            throw new RuntimeException("Failed to create GLFW Window");
+
+        //setup key callback
+        glfwSetKeyCallback(window, (window,key,scancode,action,mods) ->
+        {
+            if(key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE && debugMode)
+                glfwSetWindowShouldClose(window,true);
+        });
+
+        //Get thread stack and push new frame
+        try(MemoryStack stack = stackPush())
+        {
+            IntBuffer pWidth = stack.mallocInt(1);
+            IntBuffer pHeight = stack.mallocInt(1);
+
+            //Get window size passed to the create window method
+            glfwGetWindowSize(window, pWidth, pHeight);
+
+            //Get resolution of the primary monitor
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            //Center window on primary monitor
+            glfwSetWindowPos(
+                    window,
+                    (vidmode.width() - pWidth.get(0))/2,
+                    (vidmode.height() - pHeight.get(0))/2
+            );
+        } //popped automatically
+
+        //Temporary OpenGL setup, will become Vulkan later
+        glfwMakeContextCurrent(window);
+        //Enable v-sync
+        glfwSwapInterval(1);
+
+        //Show window since setup is finished
+        glfwShowWindow(window);
+    }
+
+    private static void update()
+    {
+        //Actual game loop
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the GLCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GL.createCapabilities();
+
+        //set the clear color
+        glClearColor(0.176f, 0.286f, 0.462f,0.0f);
+
+        //Run loop
+        while(!glfwWindowShouldClose(window))
+        {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            glfwSwapBuffers(window);
+
+            glfwPollEvents();
+        }
+    }
+}
---
+++ testing/com/fourgraphics/testing/TestGame.java
@@ -0,0 +1,11 @@
+package com.fourgraphics.testing;
+
+import com.fourgraphics.scenes.SceneManager;
+
+public class TestGame
+{
+    public static void main(String[] args)
+    {
+        SceneManager.run(1280,720,"Test Game", true);
+    }
+}
